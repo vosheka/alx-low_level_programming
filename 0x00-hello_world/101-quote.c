ab9425e2f463a0df71e8bb19a1cdadebf02fbfc6
OGOSHI OSHEKA <vosheka@gmail.com>
2022-09-08 21:36:08 -0700

    final commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - print a text followed by a new line to standard error
  *
- * Return: Always 1 (success) 
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
