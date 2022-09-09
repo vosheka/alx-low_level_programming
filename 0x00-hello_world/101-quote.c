9d8905961f34e9e75b0059973d9845a1799bc590
OGOSHI OSHEKA <vosheka@gmail.com>
2022-09-08 21:19:35 -0700

    advance task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - print a text followed by a new line to standard error
+ *
+ * Return: Always 1 (success) 
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
