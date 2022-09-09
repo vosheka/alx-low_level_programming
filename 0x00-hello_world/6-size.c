3588ab6ef1b8f688f6271df67ceeeae33aff9319
OGOSHI OSHEKA <vosheka@gmail.com>
2022-09-08 21:59:08 -0700

    another commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long ll;
 	float f;
 
-	printf("Size of a char; %id byte(s)\n", sizeof(c));
-	printf("Size of an int; %id byte(s)\n", sizeof(i));
-	printf("Size of a long int; %id byte(s)\n", sizeof(l));
-	printf("Size of a long long int; %id byte(s)\n", sizeof(ll));
-	printf("Size of a float; %id byte(s)\n", sizeof(f));
+	printf("Size of a char; %d byte(s)\n", sizeof(c));
+	printf("Size of an int; %d byte(s)\n", sizeof(i));
+	printf("Size of a long int; %d byte(s)\n", sizeof(l));
+	printf("Size of a long long int; %d byte(s)\n", sizeof(ll));
+	printf("Size of a float; %d byte(s)\n", sizeof(f));
 	return (0);
 }
