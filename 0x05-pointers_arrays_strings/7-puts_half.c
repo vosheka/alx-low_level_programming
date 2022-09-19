9c821bae3536bb023ba59bc3626866af9f61ba54
OGOSHI OSHEKA <vosheka@gmail.com>
2022-09-19 16:40:43 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,28 +8,15 @@
  */
 void puts_half(char *str)
 {
-	int j = 0;
-	int k;
+	int i;
 
-	while (str[j] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		j++;
+	_putchar(str[i]);
 	}
 
-	if (j % 2 == 1)
-	{
-		k = (j - 1) / 2;
-		k += 1;
-	}
-	else
-	{
-		k = j / 2;
-	}
-
-	for (; k < j; k++)
-	{
-		putchar(str[k]);
-	}
-
-	putchar('\n');
+	_putchar('\n');
 }
