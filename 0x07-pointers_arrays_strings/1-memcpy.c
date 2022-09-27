d5591f86abf7850dc9a8d50693c4718226f02f4d
OGOSHI OSHEKA <vosheka@gmail.com>
2022-09-27 06:11:43 -0700

    commit after correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; 1 < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		dest[i] = src{i}
+		dest[i] = src[i];
 	}
 	return (dest);
 }
